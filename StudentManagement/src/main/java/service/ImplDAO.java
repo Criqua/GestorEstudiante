523b1cbf8de44bac69d7fd5613aa2c7ee7303284
SothisIs <113253459+Criqua@users.noreply.github.com>
2023-11-28 23:09:51 -0600

    Adición de patrón de diseño DAO

---
+++ StudentManagement/src/main/java/service/ImplDAO.java
@@ -0,0 +1,98 @@
+package service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class ImplDAO implements IDAO{
+    @Override
+    public <T> List<T> getAll(String namedQuery, Class<T> clazz) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+
+        try {
+            TypedQuery<T> query = em.createNamedQuery(namedQuery, clazz);
+            return query.getResultList();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public <T> T findById(Class<T> clazz, String id) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        try {
+            T entity = em.find(clazz,id);
+            return entity;
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public <T> void remove(T entity) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        try {
+            em.getTransaction().begin();
+            em.remove(em.merge(entity));
+            em.flush();
+            em.getTransaction().commit();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public <T> void insert(T entity) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        try {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.flush();
+            em.getTransaction().commit();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public <T> T update(T entity) {
+        EntityManager em = EntityManagerAdmin.getInstance();
+        T entityUpdate = null;
+        try {
+            em.getTransaction().begin();
+            entityUpdate = em.merge(entity);
+            em.flush();
+            em.getTransaction().commit();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+        finally {
+            em.close();
+        }
+        return entityUpdate;
+    }
+}
