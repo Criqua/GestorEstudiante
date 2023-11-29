a16ad058e0b826aa7b24c37b8744943def061906
SothisIs <113253459+Criqua@users.noreply.github.com>
2023-11-28 23:15:58 -0600

    Bean inicio de sesión estudiante

---
+++ StudentManagement/src/main/java/beans/LoginBean.java
@@ -0,0 +1,36 @@
+package beans;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import service.ImplDAO;
+
+import entities.Student;
+
+@Named
+@RequestScoped
+public class LoginBean {
+
+    private String cif;
+    protected String password;
+
+    @Inject
+    private ImplDAO dao;
+
+    public String login() {
+        // Se busca al estudiante por su cif
+        Student student = dao.findById(Student.class, cif);
+
+        if (student != null && student.getPassword().equals(password)) {
+            // Si el inicio de sesión es exitoso, se almacena el CIF para futuras implementaciones
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("user", student);
+            return "loginSuccess";  // Página a la que se redirige después del inicio de sesión exitoso
+        } else {
+            // Inicio de sesión fallido, muestra un mensaje de error
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error de inicio de sesión", "CIF o contraseña incorrectos."));
+            return null;  // Permanecer en la misma página de inicio de sesión
+        }
+    }
+}
